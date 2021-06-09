145f7e0d948f659b54f1dc47085d782e3c7a56d7
boring-km <kms0644804@naver.com>
2021-06-09 23:36:47 +0900

    [master] firestore 및 google login 테스트 완료
    안드로이드는 SHA1 지문 등록이 필수이다.
    ios는 info.plist의 변경사항을 잘 확인하자. (https://eory96study.tistory.com/23)
    패키지명 bio_sns에서 biosns로 변경했다.

--- android/app/src/main/kotlin/com/kangmin/biosns/MainActivity.kt
+++ android/app/src/main/kotlin/com/kangmin/biosns/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.kangmin.bio_sns
+package com.kangmin.biosns
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:bio_sns/firebase.dart';
+import 'package:biosns/firebase.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:toast/toast.dart';
 
 void main() {
   runApp(MyApp());
@@ -30,6 +33,52 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   String _data = "test";
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  FirebaseUser currentUser;
+  String name = "";
+  String email = "";
+  String url = "";
+
+  Future<String> signIn() async {
+    final GoogleSignInAccount account = await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth = await account.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    final AuthResult authResult = await _auth.signInWithCredential(credential);
+    final FirebaseUser user = authResult.user;
+
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+
+    currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+
+    setState(() {
+      email = user.email;
+      url = user.photoUrl;
+      name = user.displayName;
+    });
+
+    return '유저: $user';
+  }
+
+  void signOut() async {
+    await _auth.signOut();
+    await googleSignIn.signOut();
+
+    setState(() {
+      email = "";
+      url = "";
+      name = "";
+    });
+
+    Toast.show("로그인 성공", context, duration: 2);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,14 +103,35 @@ class _MyHomePageState extends State<MyHomePage> {
                 _data = await FirebaseImpl.readTest();
                 setState(() {});
               },
+            ),
+            email == "" ? Container(): Column(
+              children: <Widget>[
+                Image.network(url),
+                Text(name),
+                Text(email),
+              ],
+            ),
+            RaisedButton(
+              onPressed: () {
+                if (email == "") {
+                  signIn();
+                } else {
+                  signOut();
+                }
+              },
+              child: Container(
+                width: 150,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Icon(Icons.subdirectory_arrow_right),
+                    Text(email == "" ? 'Google Login' : "Google Logout")
+                  ],
+                ),
+              ),
             )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
       ),
     );
   }
