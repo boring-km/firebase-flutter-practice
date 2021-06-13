4b3e70e7af8958be87da8ece293f4f066843cdb5
boring-km <kms0644804@naver.com>
2021-06-13 21:56:24 +0900

    [master] firebase storage 이미지를 횡스크롤로 보여줌

--- lib/screens/third.dart
+++ lib/screens/third.dart
@@ -1,13 +1,11 @@
 import 'dart:io';
 import 'package:path/path.dart';
 
-import 'package:firebase_image/firebase_image.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-/// A StatefulWidget which keeps track of the current uploaded files.
 class ThirdPage extends StatefulWidget {
   @override
   _ThirdPageState createState() => _ThirdPageState();
@@ -15,9 +13,16 @@ class ThirdPage extends StatefulWidget {
 
 class _ThirdPageState extends State<ThirdPage> {
 
-  FirebaseImage? _image;
+  Image? _image;
   File? _imageFileToUpload;
+  List<Widget> imageWidgetList = [];
   final picker = ImagePicker();
+
+  @override
+  void initState() {
+    super.initState();
+    _setImageListView();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,13 +35,19 @@ class _ThirdPageState extends State<ThirdPage> {
                 onPressed: () => setImage(),
                 child: Text('지정된 이미지 불러오기')
             ),
-            _image != null ? Image(
-              image: _image!,
-              width: 300,
-            ) : Text('이미지 아직 없음'),
+            _image != null ? _image! : Text('이미지 아직 없음'),
             ElevatedButton(
                 onPressed: () => uploadImage(context),
                 child: Text('이미지 선택하여 업로드 하기')
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20.0),
+              // 컨테이너의 높이를 200으로 설정
+              height: 300.0,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: imageWidgetList //_getImageListView(),
+              ),
             )
           ],
         ),
@@ -44,10 +55,21 @@ class _ThirdPageState extends State<ThirdPage> {
     );
   }
 
-  void setImage() {
+  void setImage() async {
+    final String url = await getImageURL('1.JPG');
     setState(() {
-      _image = FirebaseImage('gs://eco-flutter-study.appspot.com/images/1.JPG');
+      _image = Image.network(
+        url,
+      );
     });
+  }
+
+  Future<String> getImageURL(String fileName) async {
+    final ref = FirebaseStorage.instance
+        .ref()
+        .child('images/')
+        .child('$fileName');
+    return await ref.getDownloadURL();
   }
 
   void uploadImage(BuildContext context) async {
@@ -68,6 +90,32 @@ class _ThirdPageState extends State<ThirdPage> {
     setState(() {
       if (pickedFile != null)
         _imageFileToUpload = File(pickedFile.path);
+    });
+  }
+
+  void _setImageListView() async {
+
+    List<String> array = ['8e9b91f2-ce29-45c4-9e39-f1cec69c40c82569833236582256695.jpg',
+      'a5745cf5-383c-450f-802d-351bcd8d15542635813873004162341.jpg',
+      'image_picker8328229604370210524.jpg'];
+
+    List<Widget> imageCardList = [];
+    for (var path in array) {
+      final url = await getImageURL(path);
+      var widget = Container(
+        height: 300,
+        child: Column(
+          children: [
+            Image.network(url, height: 200,),
+            Text('제목'),
+            Text('작성자')
+          ],
+        ),
+      );
+      imageCardList.add(widget);
+    }
+    setState(() {
+      imageWidgetList = imageCardList;
     });
   }
 }
